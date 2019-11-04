9a1e2cadf2d125deedffc65c44e9349a931a3ed4
touyou <fujiyou1121@gmail.com>
2019-11-04 15:26:05 +0900

    setting unity

---
+++ LeapServerUnity/Assets/Scripts/FingerTouchDetector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using System.Linq;
+
+public class FingerTouchDetector : MonoBehaviour {
+
+  // Use this for initialization
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}
