e2404c9598246cfacf685b40fc27836585c49968
touyou <fujiyou1121@gmail.com>
2019-11-11 11:35:13 +0900

    setup xcode strictly

---
+++ LeapMotionClient/LeapMotionClient/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  LeapMotionClient
+//
+//  Created by 藤井陽介 on 2019/11/11.
+//  Copyright © 2019 touyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ LeapMotionClient/Pods/Target Support Files/Pods-LeapMotionClientTests/Pods-LeapMotionClientTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LeapMotionClientTests : NSObject
+@end
+@implementation PodsDummy_Pods_LeapMotionClientTests
+@end
