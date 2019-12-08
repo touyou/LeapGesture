d7572f2b56d8f762aed24b070e1eacdd7b5765d1
touyou <fujiyou1121@gmail.com>
2019-12-08 16:10:10 +0900

    add udp server and data convert

--- LeapServerUnity/Assets/Scripts/FingerTouchDetector.cs
+++ LeapServerUnity/Assets/Scripts/FingerTouchDetector.cs
@@ -11,11 +11,15 @@ public class FingerTouchDetector : MonoBehaviour {
   private Finger[] leftFingers;
   private Finger[] rightFingers;
 
+  private UdpServer server;
+
   // Use this for initialization
   void Start() {
     controller = new Controller();
     leftFingers = new Finger[5];
     rightFingers = new Finger[5];
+    server = new UdpServer();
+    server.init(5432, 1234);
   }
 
   // Update is called once per frame
@@ -29,21 +33,38 @@ public class FingerTouchDetector : MonoBehaviour {
       }
     }
 
-    Vector leapThumbTip = leftFingers[0].TipPosition;
-    float proximalLength = leftFingers[0].Bone(Bone.BoneType.TYPE_PROXIMAL).Length;
+    bool[] isLeftPinch = PinchDetect(leftFingers);
+    bool[] isRightPinch = PinchDetect(rightFingers);
+    string data = "";
+    for (int i = 0; i < isLeftPinch.Length; i++) {
+      if (isLeftPinch[i]) data += i.ToString();
+    }
+    for (int i = 0; i < isRightPinch.Length; i++) {
+      if (isRightPinch[i]) data += (i + 5).ToString();
+    }
+
+    Debug.Log(data);
+    server.send(data);
+  }
+
+  bool[] PinchDetect(Finger[] fingers) {
+    if (fingers == null || fingers.Length == 0) return new bool[0];
+    if (fingers[0] == null) return new bool[0];
+    Vector thumbTip = fingers[0].TipPosition;
+    float proximalLength = fingers[0].Bone(Bone.BoneType.TYPE_PROXIMAL).Length;
     float triggerDistance = proximalLength * TRIGGER_DISTANCE_RATIO;
 
-    bool[] isLeftPinch = new bool[4];
+    bool[] isPinch = new bool[4];
     for (int i = 1; i < HandModel.NUM_FINGERS; i++) {
-      Finger finger = leftFingers[i];
-      isLeftPinch[i - 1] = false;
-      for (int j = 0; j < FingerModel.NUM_BONES && !isLeftPinch[i - 1]; j++) {
-        Vector leapJointPosition = finger.Bone((Bone.BoneType)j).NextJoint;
-        if (leapJointPosition.DistanceTo(leapThumbTip) < triggerDistance) {
-          isLeftPinch[i - 1] = true;
+      Finger finger = fingers[i];
+      isPinch[i - 1] = false;
+      for (int j = 0; j < FingerModel.NUM_BONES && !isPinch[i - 1]; j++) {
+        Vector jointPosition = finger.Bone((Bone.BoneType)j).NextJoint;
+        if (jointPosition.DistanceTo(thumbTip) < triggerDistance) {
+          isPinch[i - 1] = true;
         }
       }
     }
-    Debug.Log(isLeftPinch[0] + "," + isLeftPinch[1] + "," + isLeftPinch[2] + "," + isLeftPinch[3]);
+    return isPinch;
   }
 }
---
+++ LeapServerUnity/Assets/Scripts/UdpServer.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UdpServer {
+  private UdpClient udpForSend;
+  private string remoteHost = "localhost";
+  private int remotePort;
+
+  public UdpServer() { }
+
+  public bool init(int port_snd, int port_to) {
+    try {
+      udpForSend = new UdpClient(port_snd);
+      remotePort = port_to;
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  public void send(string sendMsg) {
+    try {
+      byte[] sendBytes = Encoding.UTF8.GetBytes(sendMsg);
+      udpForSend.Send(sendBytes, sendBytes.Length, remoteHost, remotePort);
+    } catch { }
+  }
+
+  public void end() {
+    try {
+      udpForSend.Close();
+    } catch { }
+  }
+}
